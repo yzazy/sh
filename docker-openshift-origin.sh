58dc80a66b992691efde2e1f9e29e5bb10d9adf8
yzazy <36077646+yzazy@users.noreply.github.com>
2018-07-08 03:50:39 +0700

    Update docker-openshift-origin.sh

--- docker-openshift-origin.sh
+++ docker-openshift-origin.sh
@@ -1,2 +1,14 @@
 
+https://docs.docker.com/install/linux/docker-ce/ubuntu/
+
+sudo nano /lib/systemd/system/docker.service
+---
+ExecStart=/usr/bin/dockerd --insecure-registry 172.30.0.0/16 -H fd://
+---
+sudo systemctl daemon-reload
+
+sudo service docker start
+
+sudo chown kynuxyz /var/run/docker.sock
+
 oc cluster up --public-hostname=uop.kynu.xyz --routing-suffix='apps.kynu.xyz'  --use-existing-config=true
