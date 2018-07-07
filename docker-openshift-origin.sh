2da511248ddb2ee4878af543db69145daa93baa0
yzazy <36077646+yzazy@users.noreply.github.com>
2018-07-08 03:40:16 +0700

    Update docker-openshift-origin.sh

--- docker-openshift-origin.sh
+++ docker-openshift-origin.sh
@@ -1,13 +1,2 @@
-#!/bin/sh
-set -e
-# This script is meant for quick & easy install via:
-# docker
-curl -fsSL get.docker.com -o get-docker.sh
-sh get-docker.sh
-# on ubuntu
-ExecStart=/usr/bin/dockerd --insecure-registry 172.30.0.0/16 -H fd://
-sudo systemctl daemon-reload
-sudo systemctl restart docker
-# oc run
-export OC_CLUSTER_PUBLIC_HOSTNAME=127.0.0.1
-oc-cluster up
+
+oc cluster up --public-hostname=uop.kynu.xyz --routing-suffix='apps.kynu.xyz'  --use-existing-config=true
